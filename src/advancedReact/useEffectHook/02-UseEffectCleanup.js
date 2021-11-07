426d932a3cdf3c8aad95e33a65ba4524b7ac36aa
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-08 01:48:45 +0330

    UseEffect Cleanup

--- src/advancedReact/useEffectHook/02-UseEffectCleanup.js
+++ src/advancedReact/useEffectHook/02-UseEffectCleanup.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+//* Check size of the window(width)
 
 const UseEffectCleanup = () => {
-  return <div></div>;
+  const [size, setSize] = useState(window.innerWidth);
+  const resizeHandler = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log('effect');
+    window.addEventListener('resize', resizeHandler);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', resizeHandler);
+    };
+  });
+  console.log('render');
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
